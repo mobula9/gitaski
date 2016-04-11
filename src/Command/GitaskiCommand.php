6c0abbd5a49d01b5914720f7f834c878faee606f
Lucas CHERIFI <lucas@cherifi.info>
2016-04-11 23:12:34 +0200

    Refacto

---
+++ src/Command/GitaskiCommand.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Kasifi\Gitaski\Command;
+
+use Exception;
+use Kasifi\Gitaski\Gitaski;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Yaml\Yaml;
+
+class GitaskiCommand extends Command
+{
+    /**
+     * return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('gitaski')
+            ->setDescription('Build Github ASCII art')
+            ->addArgument(
+                'github_repository_url',
+                InputArgument::OPTIONAL,
+                'Which github repository URL?'
+            )
+            ->addOption(
+                'use_text',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'If set, the ascii art will be generated from this text.'
+            )
+            ->addOption(
+                'artwork_path',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'If set, the JSON file at this path will be used instead of the text.'
+            )
+            ->addOption(
+                'input_filepath',
+                'f',
+                InputOption::VALUE_REQUIRED,
+                'If set, this file will be used to generate dummy content.',
+                realpath(__DIR__ . '/../Resources/fixtures/sample.md')
+            )
+            ->addOption(
+                'commit_list_yml_filepath',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'If set, this file will be used to generate dummy content.',
+                realpath(__DIR__ . '/../Resources/fixtures/commit-messages.yml')
+            )
+            ->addOption(
+                'output_filename',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'If set, this filename will be used for the commited file.',
+                'README.md'
+            )
+            ->addOption(
+                'force',
+                false,
+                InputOption::VALUE_NONE,
+                'If set, the commits will be really pushed to the repository URL, else it run in dry-mode mode.'
+            );
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     * @throws Exception
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $force = $input->getOption('force');
+        $githubRepositoryUrl = $input->getArgument('github_repository_url');
+        $inputFilePath = $input->getOption('input_filepath');
+        if (!file_exists($inputFilePath)) {
+            throw new Exception($inputFilePath . ' file was not found.');
+        }
+        $outputFilename = $input->getOption('output_filename');
+
+        $commitsMessageYmlPath = $input->getOption('commit_list_yml_filepath');
+        if (!file_exists($commitsMessageYmlPath)) {
+            throw new Exception($commitsMessageYmlPath . ' file was not found.');
+        }
+        $commitMessages = Yaml::parse(file_get_contents($commitsMessageYmlPath));
+
+        $gitaski = new Gitaski($force, $githubRepositoryUrl, $inputFilePath, $outputFilename, $commitMessages);
+        $gitaski->setIo($io);
+
+        $useText = $input->getOption('use_text');
+        if ($useText) {
+            $gitaski->writeText($useText);
+        } else {
+            $artworkPath = $input->getOption('artwork_path');
+            if (!$artworkPath) {
+                throw new \InvalidArgumentException('Either "use_text" or "artwork_path" option should be defined');
+            }
+            $gitaski->writeJson($artworkPath);
+        }
+        $gitaski->clean();
+    }
+}
---
+++ src/GitAski.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Kasifi\Gitaski;
+
+use DateTime;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Yaml\Yaml;
+
+class Gitaski
+{
+    /** @var SymfonyStyle */
+    private $io;
+
+    /**
+     * Gitaski constructor.
+     *
+     * @param boolean $force
+     * @param string  $githubRepositoryUrl
+     * @param         $inputFilePath
+     * @param         $outputFilename
+     * @param array   $commitMessages
+     */
+    public function __construct($force, $githubRepositoryUrl, $inputFilePath, $outputFilename, array $commitMessages)
+    {
+        $this->gitHelper = new GitHelper($githubRepositoryUrl, $force, $inputFilePath, $outputFilename, $commitMessages);
+
+        $date = new DateTime();
+        $date->setTime(12, 0, 0);
+        $this->lastSunday = $this->gitHelper->getPreviousSunday($date);
+    }
+
+    /**
+     * @param SymfonyStyle $io
+     */
+    public function setIo($io)
+    {
+        $this->io = $io;
+        $this->gitHelper->setIo($this->io);
+    }
+
+    public function writeText($str)
+    {
+        $this->gitHelper->initLocalRepository();
+
+        $asciiString = AsciiHelper::generateAsciiFromText($str);
+
+        $this->io->note('Ascii string received:');
+        $this->io->write($asciiString);
+
+        $symbol = AsciiHelper::generateSymbolFromAsciiString($asciiString, ['#' => 2, ' ' => 1]);
+
+        $this->write($symbol);
+    }
+
+    public function writeJson($filename)
+    {
+        $json = json_decode(file_get_contents(__DIR__ . '/' . $filename), true);
+        $symbol = AsciiHelper::generateSymbolFromJson($json, [
+            '#eeeeee' => 0,
+            '#cdeb8b' => 1,
+            '#6bba70' => 2,
+            '#009938' => 3,
+            '#006e2e' => 4,
+        ]);
+        $this->write($symbol);
+    }
+
+    public function clean() {
+        $this->gitHelper->clean();
+    }
+
+    /**
+     * @param $symbol
+     */
+    private function write($symbol)
+    {
+        $this->io->note('Symbol to draw');
+        $this->io->write(AsciiHelper::renderSymbol($symbol));
+        $this->gitHelper->writeSymbol($symbol, $this->lastSunday);
+    }
+}
