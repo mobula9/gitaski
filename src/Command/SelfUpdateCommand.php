74946fc11368ef97b23cd4e9cd3cb72453c6f7c7
Lucas CHERIFI <lucas@cherifi.info>
2016-04-16 10:31:22 +0200

    Code style

--- src/Command/SelfUpdateCommand.php
+++ src/Command/SelfUpdateCommand.php
@@ -3,11 +3,10 @@
 namespace Kasifi\Gitaski\Command;
 
 use Exception;
+use Humbug\SelfUpdate\Strategy\ShaStrategy;
 use Humbug\SelfUpdate\Updater;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -32,25 +31,27 @@ class SelfUpdateCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $io = new SymfonyStyle($input, $output);
 
         $updater = new Updater();
-        $updater->getStrategy()->setPharUrl('http://lucascherifi.github.io/gitaski/gitaski.phar');
-        $updater->getStrategy()->setVersionUrl('http://lucascherifi.github.io/gitaski/gitaski.phar.version');
-        $io = new SymfonyStyle($input, $output);
-        try {
-            $result = $updater->update();
-            if (!$result) {
-                $io = new SymfonyStyle($input, $output);
-                $io->success('No update needed.');
-                exit(0);
-            }
-            $new = $updater->getNewVersion();
-            $old = $updater->getOldVersion();
-            $io->success(sprintf('Updated from %s to %s', $old, $new));
-            exit(0);
-        } catch (\Exception $e) {
-            $io->error($e->getMessage());
-            exit(1);
+        $strategy = $updater->getStrategy();
+
+        if ($strategy instanceof ShaStrategy) {
+            $strategy->setPharUrl('http://lucascherifi.github.io/gitaski/gitaski.phar');
+            $strategy->setVersionUrl('http://lucascherifi.github.io/gitaski/gitaski.phar.version');
         }
+
+        $result = $updater->update();
+        if (!$result) {
+            $io = new SymfonyStyle($input, $output);
+            $io->success('No update needed.');
+
+            return;
+        }
+        $new = $updater->getNewVersion();
+        $old = $updater->getOldVersion();
+        $io->success(sprintf('Updated from %s to %s', $old, $new));
+
+        return;
     }
 }
